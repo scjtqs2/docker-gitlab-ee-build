ae76a52f8ab8ce2a842b592ad3f28265c0c12df8
scjtqs <scjtqs@qq.com>
2024-05-16 18:26:37 +0800

    fix: 编译arm64和amd64镜像

--- build.sh
+++ build.sh
@@ -30,17 +30,13 @@ done < latest
 for LATEST in "${no_match_tags[@]}"; do
     echo "Processing tag: ${LATEST}"
     echo $LATEST >> version
-    git clone https://gitlab.com/gitlab-org/omnibus-gitlab.git
-    cd omnibus-gitlab/docker
     echo "PACKAGECLOUD_REPO=gitlab-ee" > RELEASE
     echo "RELEASE_PACKAGE=gitlab-ee" >> RELEASE
     echo "RELEASE_VERSION=${LATEST}" >> RELEASE
     echo "DOWNLOAD_URL_amd64=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_amd64.deb/download.deb" >> RELEASE
     echo "DOWNLOAD_URL_arm64=https://packages.gitlab.com/gitlab/gitlab-ee/packages/ubuntu/focal/gitlab-ee_${LATEST}_arm64.deb/download.deb" >> RELEASE
-#    sed -i 's/\-recommends/\-recommends libatomic1/' Dockerfile
-#    docker run --privileged --rm tonistiigi/binfmt --install all
-    docker buildx  build --platform  linux/arm64  -t ${DOCKER_NAME}/gitlab-ee-arm64:${LATEST} --build-arg TARGETARCH=arm64 -f Dockerfile --push . || exit 1
-    cd ../../
+    docker buildx  build --platform  linux/arm64,linux/amd64  -t ${DOCKER_NAME}/gitlab-ee:${LATEST}  -f Dockerfile --push . || exit 1
+    rm RELEASE
     git add latest
     git add version
     git config --local user.email ${MAIL}
